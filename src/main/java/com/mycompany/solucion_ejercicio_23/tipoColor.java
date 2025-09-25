1e97b068080be07ee99fb430b0406fa8df0dbad6
Migue <Migue@LAPTOP-H5CIMOMH>
2025-09-25 00:38:02 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/solucion_ejercicio_23/tipoColor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.mycompany.solucion_ejercicio_23;
+
+/**
+ *
+ * @author Migue
+ */
+public enum tipoColor {
+BLANCO, 
+NEGRO, 
+ROJO, 
+NARANJA,
+AMARILLO,
+VERDE, 
+AZUL, 
+VIOLETA
+}
+
+
